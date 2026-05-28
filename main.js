72cf1ad4dbc7b10a6489603ba141f30e80b5964a
seanmapuke <seanmapuke1@gmail.com>
2026-05-28 11:45:24 -0500

    new token

--- main.js
+++ main.js
@@ -104,7 +104,7 @@ if (spClose) {
 const DEMO_MODE     = false;
 const CLIENT_ID     = '4e66114e764044b4a42eae803d34c038';
 const CLIENT_SECRET = '038568c332a8407db12386838fb84702';
-const REFRESH_TOKEN = 'AQC-gqWdGlCHePqP6rHvLzFq1RpWPR_BfzVHrFN0EQpCtt0I5Ir-yM4PYewaxHTADqhuxqy_6MjfM6DZkbzzdVdK9hJez-LHnB7ymHJqgEwBS2JGOb-KnqRG6GEl0yDU4jY';
+const REFRESH_TOKEN = 'AQCCHdrU1fEsgROuu2I7m2GRzOT_WjZ63kDX4uwMR9liEmuOpjk7HtdGvw8vToK6jq0PnrXQgsOvGHrMtBis8UHb91pOTdeH8xfTCpCAwip_q2psWWRFntZX1bwey6q4Ohk';
 
 let spotifyOpen = false;
 
